2c88a2247faa15d3697376c65cf3f44d4fcf46b5
KineOnes <kine.verhaaf@gmail.com>
2024-01-26 20:58:27 +0100

    added images and about page

---
+++ js/index.js
@@ -0,0 +1,10 @@
+import { contactPage } from "./pages/contactPage.js";
+import { homepge } from "./pages/contactPage.js"
+
+// We have to make sure that the right code fires on the right page
+
+// Router
+
+if (window.location.pathname === "home.html") {
+    homePage()
+}
