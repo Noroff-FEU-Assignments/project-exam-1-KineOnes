82148b681979af419028e3a77df0c8956dc32f26
KineOnes <kine.verhaaf@gmail.com>
2024-02-08 16:53:53 +0100

    adjusted text in slideshow

--- scripts/get_blog_posts.js
+++ scripts/get_blog_posts.js
@@ -1,9 +1,6 @@
 const baseUrl = "http://onesdesign.local/wp-json/wp/v2/posts?_embed";
 const postsContainer = document.querySelector(".posts");
 
-//Loader - NOT FINISHED//
-//document.querySelector('.posts').innerHTML = `<div class="loader"> <h1>Loading...</h1></div>`;//
-
 async function getBlogPosts(url){
     const response = await fetch(url);
     const posts = await response.json();
@@ -19,6 +16,7 @@ async function getBlogPosts(url){
                     <a class="button" href="contact.html">CONTACT</a> 
                 </div>
             </div>`;
+
     })
 }
 
